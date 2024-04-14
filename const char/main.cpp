dd3a3daca191e4175fbfb71fcf2ea15d8b50ef3c
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-04-13 23:20:16 -0300

    const char*

---
+++ const char/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+const char *fulano= "eu gosto de c++";
+
+cout << fulano << endl;
+
+const char*comando = "pause";
+
+system(comando);
+return 0;
+}
