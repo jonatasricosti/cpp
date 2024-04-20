dfb068a98f486ed0d1c0e715f16f121795c69793
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-04-19 23:39:38 -0300

    return

---
+++ return/return.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// o comando return dentro de uma função
+// sai da função
+
+void teste()
+{
+	return;
+	cout << "ola mundo" << endl;
+}
+
+int main()
+{
+teste();
+
+system("pause");
+return 0;
+}
