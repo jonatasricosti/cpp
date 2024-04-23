afc76e7b7a67066317cb85ac34c0e5007991a788
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-04-22 23:21:35 -0300

    setlocale

---
+++ setlocale/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+setlocale(LC_ALL, "portuguese"); // serve pra usar caracteres brasileiros 
+
+cout << "olá, mundo!" << endl;
+
+system("pause");
+return 0;
+}
