83266f4ce5459004b407e17e278f0ddeaf711e58
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-05-07 23:48:09 -0300

    prinft

---
+++ prinft/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+
+char letra = 'j';
+char nome[] = "Jonatas"; // %s
+int idade = 20; // %i
+float meufloat = 2.5; // %f
+double meudouble = 3.141592; // %lf
+
+printf("letra %c", letra);
+printf("\n"); // pula linha
+printf("%s tem %i de idade", nome, idade);
+printf("\n"); // pula linha
+printf("float %f", meufloat);
+printf("\n"); // pula linha
+printf("double %lf", meudouble);
+printf("\n"); // pula linha
+
+system("pause");
+return 0;
+}
