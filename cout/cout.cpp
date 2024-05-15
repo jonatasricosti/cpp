044d8baf08316732a738b8f8376d7c5426b8a4bc
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-05-14 22:58:10 -0300

    cout

---
+++ cout/cout.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // pra usar o comando cout
+
+int main()
+{
+setlocale(LC_ALL, "portuguese");// serve pra usar caracteres brasileiros
+
+// o cout é comando escreva da lingaugem c++
+// o cout = console out serve pra escrever na tela do terminal
+
+
+std::cout << "olá, mundo" << std::endl;
+
+using namespace std; // com isso não precisa escrever std::
+
+cout << "oi, mundo" << endl;
+
+
+system("pause");
+return 0;
+}
