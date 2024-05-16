611b187102d4a081de36a20be75213fc8504beec
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-05-15 22:50:46 -0300

    cin

---
+++ cin/cin.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // pra usar o comando cout
+using namespace std; // com isso não precisa escrever std::
+
+int main()
+{
+setlocale(LC_ALL, "portuguese");// serve pra usar caracteres brasileiros
+
+// o cin é comando leia da lingaugem c++
+// o cin = character ou console input serve pra ler dados
+
+
+cout << "digite um número: ";
+int valor;
+cin >> valor; // coloque o valor nessa variável
+cout << "você digitou o número: " << valor << endl;
+
+
+system("pause");
+return 0;
+}
