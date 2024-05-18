38191a4b3c88eb374c966f6d66b5ab8ab7c21b7a
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-05-17 23:06:09 -0300

    int main

---
+++ int main/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // pra usar o comando cout
+
+// a função int main ou função principal é onde nosso programa começa a ser executado
+int main()
+{
+// começo do programa
+// aqui em baixo, começa ser executado nosso programa em c ou c++
+
+
+setlocale(LC_ALL, "portuguese");// serve pra usar caracteres brasileiros
+
+std::cout << "Eu amo c++" << std::endl;
+
+
+system("pause");
+return 0;
+// fim do programa
+// aqui é o fim do programa, ou seja, onde o nosso programa é fechado
+}
