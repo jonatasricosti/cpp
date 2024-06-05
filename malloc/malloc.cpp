18902198e76e2709073b0f853a5c8d1e9a45c33d
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-06-04 22:46:59 -0300

    malloc

---
+++ malloc/malloc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+setlocale(LC_ALL, "portuguese");
+
+srand(time(NULL));
+
+
+cout << "qual vai ser a quantidade de elementos do seu array: ";
+int n_elementos;
+cin >> n_elementos;
+
+
+// a função malloc serve pra fazer alocação dinâmica de memória
+// a função malloc retorna void, por isso temos que fazer c type casting
+int *array = (int *)  malloc(n_elementos * sizeof(int)); // aloca um bloco de memória na heap
+
+for(int i = 0; i < n_elementos; i++)
+{
+	array[i] = rand() % 16; // gera números aleatórios de 0 até 15
+	
+	cout << array[i] << endl;
+}
+
+free(array); // desaloca um bloco de memória
+
+system("pause");
+return 0;
+}
