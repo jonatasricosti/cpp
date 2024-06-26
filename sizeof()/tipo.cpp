75bf297705729c033a261078b3392521e9162ab0
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-06-25 23:00:49 -0300

    sizeof()

---
+++ sizeof()/tipo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+// A função sizeof(tipo) retorna o tamanho em bytes do tipo
+
+cout << sizeof(int) << " bytes" << endl;
+cout << sizeof(double) << " bytes" << endl;
+
+system ("pause");
+return 0;
+}
