7ae19a8a5017ecc3c297a07815d1c7a0414206d4
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-08 23:17:22 -0300

    mútiplos ifs vs if, else if, else

---
+++ mútiplos ifs vs if, else if, else/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+// múltiplos if vs if, else if, else
+
+int main()
+{
+int x = 5;
+
+if(x > 1) cout << "eu" << endl;
+if(x > 2) cout << "ele"  << endl;
+if(x > 3) cout << "ela"  << endl;
+// quando temos múltiplos if todos eles não executados
+
+cout << "\n\n";
+
+// quando não temos múltiplos if, ou seja, temos else if ou else somente um código é executado
+
+if(x > 1) cout << "meu" << endl;
+else if(x > 2) cout << "dele"  << endl; // precisa do if anterior
+else  cout << "dela"  << endl;          // se não foi if ou else if execute isso
+
+system("pause");
+return 0;
+}
